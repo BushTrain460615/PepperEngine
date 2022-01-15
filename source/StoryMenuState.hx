069ac466aea75c70593d7ca01437c410a9098057
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-01-15 01:25:33 -0500

    ghgghfdbgdjbgdkbgs

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -34,7 +34,7 @@ class StoryMenuState extends MusicBeatState
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, false];
 
 	var weekCharacters:Array<Dynamic> = [
 		['dad', 'bf', 'gf'],
